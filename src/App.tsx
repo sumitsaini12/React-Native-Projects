db2de9847aceed107701519ef604d2bfca4952c9
sumit kumar <sumitsaini95488@gmail.com>
2023-12-25 22:59:09 +0530

    create currency converter App

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import BgColorChange from './Projects/bgColorChange/BgColorChange';
 import PasswordGeneratorApp from './Projects/PasswordGenerator/PasswordGeneratorApp';
 import RollTheDicesApp from './Projects/rollTheDices/RollTheDicesApp';
+import CurrencyConverterApp from './Projects/currencyConverter/CurrencyConverterApp';
 function App() {
   return (
     <>
       {/* <PasswordGeneratorApp /> */}
       {/* <BgColorChange /> */}
-      <RollTheDicesApp />
+      {/* <RollTheDicesApp /> */}
+      <CurrencyConverterApp />
     </>
   );
 }
---
+++ src/Projects/currencyConverter/CurrencyConverterApp.tsx
@@ -0,0 +1,130 @@
+import React, {useState} from 'react';
+import {
+  FlatList,
+  Pressable,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+import {currencyByRupee} from './constants';
+import CurrencyButton from './components/CurrencyButton';
+import Snackbar from 'react-native-snackbar';
+
+export default function CurrencyConverterApp() {
+  const [inputValue, setInputValue] = useState('');
+  const [resultValue, setResultValue] = useState('');
+  const [targetCurrency, setTargetCurrency] = useState('');
+
+  const buttonPressed = (targetValue: Currency) => {
+    if (!inputValue) {
+      return Snackbar.show({
+        text: 'Enter a value to convert',
+        backgroundColor: '#EA7773',
+        textColor: '#000000',
+      });
+    }
+
+    const inputAmount = parseFloat(inputValue);
+    if (!isNaN(inputAmount)) {
+      const convertedValue = inputAmount * targetValue.value;
+      const result = `${targetValue.symbol} ${convertedValue.toFixed(2)}`;
+      setResultValue(result);
+      setTargetCurrency(targetValue.name);
+    } else {
+      return Snackbar.show({
+        text: 'Not a valid number to convert',
+        backgroundColor: '#F4BE2C',
+        textColor: '#000000',
+      });
+    }
+  };
+
+  return (
+    <>
+      <StatusBar />
+      <View style={styles.container}>
+        <View style={styles.topContainer}>
+          <View style={styles.rupeesContainer}>
+            <Text style={styles.rupee}>₹</Text>
+            <TextInput
+              maxLength={14}
+              value={inputValue}
+              clearButtonMode="always" //only for iOS
+              onChangeText={setInputValue}
+              keyboardType="number-pad"
+              placeholder="Enter amount in Rupees"
+            />
+          </View>
+          {resultValue && <Text style={styles.resultTxt}>{resultValue}</Text>}
+        </View>
+        <View style={styles.bottomContainer}>
+          <FlatList
+            numColumns={3}
+            data={currencyByRupee}
+            keyExtractor={item => item.name}
+            renderItem={({item}) => (
+              <Pressable
+                style={[
+                  styles.button,
+                  targetCurrency === item.name && styles.selected,
+                ]}
+                onPress={() => buttonPressed(item)}>
+                <CurrencyButton {...item} />
+              </Pressable>
+            )}
+          />
+        </View>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#515151',
+  },
+  topContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+  rupeesContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  rupee: {
+    marginRight: 8,
+    fontSize: 22,
+    color: '#000000',
+    fontWeight: '800',
+  },
+  resultTxt: {
+    fontSize: 32,
+    color: '#000000',
+    fontWeight: '800',
+  },
+  bottomContainer: {
+    flex: 3,
+  },
+  button: {
+    flex: 1,
+    margin: 12,
+    height: 60,
+    borderRadius: 12,
+    backgroundColor: '#fff',
+    elevation: 2,
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+    shadowColor: '#333',
+    shadowOpacity: 0.1,
+    shadowRadius: 1,
+  },
+  selected: {
+    backgroundColor: '#ffeaa7',
+  },
+});
